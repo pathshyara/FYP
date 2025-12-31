8cd5c86881e196f9602ff40a05d78fc55918bf33
pathshyara <pathshyara_phetch@slcp.uum.edu.my>
2025-12-31 13:33:59 +0800

    update initial commit

--- frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { TranslatorComponent } from './components/translator/translator.component';
-
-export const routes: Routes = [
-  { path: '', component: TranslatorComponent },
-  { path: 'translator', component: TranslatorComponent },
-];
